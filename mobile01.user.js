0a7cbff6a9b07d795bdec6f3aa917fd39eb58f3e
anenasa <anenasaa@yahoo.com>
2024-10-14 21:58:08 +0800

    Initial commit

---
+++ mobile01.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Mobile01 自動前往外部連結
+// @namespace   Violentmonkey Scripts
+// @match       https://www.mobile01.com/externallink.php*
+// @grant       none
+// @version     1.0
+// @author      anenasa
+// @description 2024/9/23 上午9:31:18
+// ==/UserScript==
+if(window.location.pathname == "/externallink.php"){
+  params = new URLSearchParams(window.location.search);
+  document.location = params.get("url");
+}
