8432ae079d75dc8c4daeab1c1768b6507568b791
anenasa <84259093+anenasa@users.noreply.github.com>
2024-12-19 15:25:32 +0800

    Update ptt.user.js

--- ptt.user.js
+++ ptt.user.js
@@ -3,7 +3,7 @@
 // @namespace   anenasa
 // @match       https://www.ptt.cc/bbs/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      anenasa
 // @description 2024/12/19 下午1:47:19
 // ==/UserScript==
@@ -15,13 +15,23 @@
     if(href.includes("i.imgur.com/")){
       src = href;
     }
+    else if(href.includes("imgur.com/")){
+      index = href.indexOf("imgur.com");
+      src = href.substring(0, index) + "i." + href.substring(index);
+      if(!href.includes(".", index + 10)){
+        // Append extension to link
+        // Assume extension is .jpg,
+        // should work even if extension is actually png
+        src += ".jpg";
+      }
+    }
     else if(href.includes("pbs.twimg.com/media/") && href.includes("?")){
       src = href;
     }
 
     if(src){
       img = document.createElement("img");
-      img.setAttribute("src", href);
+      img.setAttribute("src", src);
       if(src.includes("i.imgur.com/")){
         img.referrerPolicy="no-referrer";
       }
