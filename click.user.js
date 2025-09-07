985c0287d2240717ae0f5d148783d5a7e8f449ec
anenasa <84259093+anenasa@users.noreply.github.com>
2025-09-07 09:52:06 +0800

    Update click.user.js

--- click.user.js
+++ click.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://*/*
 // @grant       none
-// @version     1.5
+// @version     1.6
 // @author      -
 // @description 2025/9/2 下午1:45:43
 // ==/UserScript==
@@ -13,13 +13,15 @@ let size = 20 / zoom  + "vmin";
 
 let btnDown = document.createElement("div");
 btnDown.style.position = "fixed";
-btnDown.style.top = "40vh";
+btnDown.style.bottom = "50vh";
 btnDown.style.left = "0";
 btnDown.style.width = size;
 btnDown.style.height = size;
 btnDown.style.border = "thin solid black";
 btnDown.style.zIndex = "9999";
-btnDown.addEventListener("click", btnDown_click, false);
+btnDown.addEventListener("click", function(){
+  window.scrollBy(0, window.innerHeight * 0.9 * zoom);
+});
 document.body.appendChild(btnDown);
 
 let btnUp = document.createElement("div");
@@ -30,15 +32,20 @@ btnUp.style.width = size;
 btnUp.style.height = size;
 btnUp.style.border = "thin solid black";
 btnUp.style.zIndex = "9999";
-btnUp.addEventListener("click", btnUp_click, false);
+btnUp.addEventListener("click", function(){
+  window.scrollBy(0, -window.innerHeight * 0.9 * zoom);
+});
 document.body.appendChild(btnUp);
 
-function btnDown_click(){
-  console.log("click");
-  window.scrollBy(0, window.innerHeight * 0.9 / zoom);
-}
-
-function btnUp_click(){
-  console.log("click");
-  window.scrollBy(0, -window.innerHeight * 0.9 / zoom);
-}
+let btnBack = document.createElement("div");
+btnBack.style.position = "fixed";
+btnBack.style.top = "50vh";
+btnBack.style.left = "0";
+btnBack.style.width = size;
+btnBack.style.height = size;
+btnBack.style.border = "thin solid black";
+btnBack.style.zIndex = "9999";
+btnBack.addEventListener("click", function(){
+  history.back();
+});
+document.body.appendChild(btnBack);
