0d40dc37b4d5ceaeab3554da7cd81413ed006df3
anenasa <84259093+anenasa@users.noreply.github.com>
2025-10-11 09:52:04 +0800

    Update click.user.js

--- click.user.js
+++ click.user.js
@@ -3,17 +3,17 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://*/*
 // @grant       none
-// @version     1.7
+// @version     1.8
 // @author      -
 // @description 2025/9/2 下午1:45:43
 // ==/UserScript==
 
-let zoom = window.innerWidth / document.documentElement.scrollWidth;
+let zoom = window.innerWidth / document.body.scrollWidth;
 let size = 20 / zoom  + "vmin";
 
 window.addEventListener("resize", function () {
   // Original zoom is wrong if page is open in new tab
-  zoom = window.innerWidth / document.documentElement.scrollWidth;
+  zoom = window.innerWidth / document.body.scrollWidth;
   size = 20 / zoom  + "vmin";
   btnDown.style.width = size;
   btnDown.style.height = size;
@@ -25,33 +25,33 @@ window.addEventListener("resize", function () {
 
 let btnDown = document.createElement("div");
 btnDown.style.position = "fixed";
-btnDown.style.bottom = "50vh";
+btnDown.style.bottom = 50 / zoom + "vh";
 btnDown.style.left = "0";
 btnDown.style.width = size;
 btnDown.style.height = size;
 btnDown.style.border = "thin solid black";
 btnDown.style.zIndex = "9999";
 btnDown.addEventListener("click", function(){
-  window.scrollBy(0, window.innerHeight * 0.9 * zoom);
+  window.scrollBy(0, window.innerHeight * 0.9);
 });
 document.body.appendChild(btnDown);
 
 let btnUp = document.createElement("div");
 btnUp.style.position = "fixed";
-btnUp.style.top = "40vh";
+btnUp.style.top = 40 / zoom + "vh";
 btnUp.style.right = "0";
 btnUp.style.width = size;
 btnUp.style.height = size;
 btnUp.style.border = "thin solid black";
 btnUp.style.zIndex = "9999";
 btnUp.addEventListener("click", function(){
-  window.scrollBy(0, -window.innerHeight * 0.9 * zoom);
+  window.scrollBy(0, -window.innerHeight * 0.9);
 });
 document.body.appendChild(btnUp);
 
 let btnBack = document.createElement("div");
 btnBack.style.position = "fixed";
-btnBack.style.top = "50vh";
+btnBack.style.top = 50 / zoom + "vh";
 btnBack.style.left = "0";
 btnBack.style.width = size;
 btnBack.style.height = size;
