fbca8a7594b321716d0358e945fabff2666de8fc
anenasa <84259093+anenasa@users.noreply.github.com>
2025-10-17 22:09:42 +0800

    Update button.user.js

--- button.user.js
+++ button.user.js
@@ -3,14 +3,13 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://*/*
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      -
 // @description 2025/10/17 下午8:33:30
 // ==/UserScript==
 
 (function() {
     document.addEventListener("keydown", (event) => {
-      console.log(event.key);
       if (event.keyCode == 13) {
         event.preventDefault();
         window.scrollBy(0, window.innerHeight * 0.9);
